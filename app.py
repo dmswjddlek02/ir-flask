29fdf4a3e650786f6d042f819c74b70dd9e20f13
E.Jung <kawaii_san2@naver.com>
2025-07-16 20:36:42 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    files = request.files.getlist("files[]")
+    dfs = []
+    for file in files:
+        df = pd.read_excel(file)
+        dfs.append(df)
+    combined = pd.concat(dfs, ignore_index=True)
+    return jsonify(combined.head(10).to_dict(orient="records"))
+
+if __name__ == "__main__":
+    print("flask 서버 시작 중...")
+    app.run(debug=True)
